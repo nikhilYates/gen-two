40eaa26595066d3c0f2a32277c88e169240e8152
nikhilYates <nikhil.yates@outlook.com>
2024-11-06 22:54:41 -0500

    initial commit/design

---
+++ nikhilyates-two/src/app/landing/LandingPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='bg-gradient-to-tr from-black to bg-gray-900 h-screen'>
+        <div className='h-full flex flex-col justify-between p-8'>
+            <div>
+                <h1 className='text-white text-xl font-mono'>njy</h1>
+            </div>
+            <div>
+                <h1 className='text-white text-[10rem] font-mono'>nikhil yates</h1>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default LandingPage
