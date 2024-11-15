94b0ddcf90f187d1d9727279c58a2c4548255b0a
nikhilYates <nikhil.yates@outlook.com>
2024-11-15 00:54:29 -0500

    redefining style for landing page

--- nikhilyates-two/src/app/landing/components/NavCard.tsx
+++ nikhilyates-two/src/app/landing/components/NavCard.tsx
@@ -11,8 +11,8 @@ const NavCard = ({ content }: NavCardProps) => {
   return (
     <div 
         key={content.id} 
-        className='opacity-50 rounded-2xl w-[10rem] h-[34rem] overflow-hidden flex flex-col items-start justify-end 
-        border-2 border-gray-400 cursor-pointer transition-all duration-1000 hover:w-[500px] hover:opacity-100'
+        className='opacity-50 rounded-2xl w-[10rem] h-[22rem] overflow-hidden flex flex-col items-start justify-end 
+        border-[0.25rem] border-gray-400 cursor-pointer transition-all duration-1000 hover:w-[22rem] hover:opacity-100'
     >
         <div className='transform whitespace-nowrap text-left p-4'>
             <h3 className="text-6xl font-semibold tracking-tight text-white">{content.title}</h3>
