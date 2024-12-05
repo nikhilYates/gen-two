e5fc9a094842deb225d88955aa63dbb014762577
nikhilYates <nikhil.yates@outlook.com>
2024-12-04 23:44:43 -0500

    about section done

--- nikhilyates-two/src/app/about/page.tsx
+++ nikhilyates-two/src/app/about/page.tsx
@@ -3,14 +3,16 @@ import react from 'react'
 const About = () => {
 
     return (
-        <div className='bg-zinc-950 h-screen p-16'>
-            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-zinc-500 leading-[4rem]'><span className='text-white'>nikhil yates</span> is a <span className='text-white'>
-            versatile full-stack engineer and entrepreneur </span> based in <span className='text-white'>Toronto.</span>
-             With a solid foundation in computer science and a knack for solving complex problems across various domains, nikhil's
-            interests are unbounded</h1>
-            <br />
-            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-zinc-500 leading-10'>
-                anyways, i like building stuff, designing stuff, learning new stuff. how's that for professional
+        <div className='bg-zinc-950 h-screen p-16 flex flex-col justify-between'>
+            <h1 className='scroll-m-20 text-8xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-[8rem]'><span className='text-white'>nikhil yates</span> is a <span className='text-white'>
+            versatile full-stack engineer and entrepreneur </span> based in <span className='text-white'>Toronto. </span>
+             With a solid foundation in computer science and a knack for <span className='text-white'>solving complex problems</span> across <span className='text-white'>various domains</span>, nikhil's
+            interests span <span className='text-white'> web3, generative ai, data interactivity, robotics, and UI.</span></h1>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-10'>
+                <span className='text-white'>TLDR: </span> i like <span className='text-white'>stuff(building + designing + learning)</span>. how's that for professional
+            </h1>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-10'>
+                want to collaborate? let's get in <span className='underline text-white'>contact.</span>
             </h1>
 
         </div>
