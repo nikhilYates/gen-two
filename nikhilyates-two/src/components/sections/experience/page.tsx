21cf12f74f4d394c2452425dd915452c95b6e58b
nikhilYates <nikhil.yates@outlook.com>
2024-12-05 00:03:04 -0500

    starting experience migration to json file

--- nikhilyates-two/src/components/sections/experience/page.tsx
+++ nikhilyates-two/src/components/sections/experience/page.tsx
@@ -3,22 +3,15 @@ import { ExperienceType } from '@/lib/definitions'
 import ExperienceCardLarge from './utilities/ExperienceCard'
 import ExperienceCardSmall from './utilities/ExperienceCardSmall'
 
-
-// RAG-interactable content here. want to be able to select an experience, 
-// interact with it using an LLM + perform a compare/contrast with another experience
-// sort experiences by key skills developed, tools used, volunteer work, etc
-
 const dummyExperience: ExperienceType = {
   title: "Full-Stack Engineer",
   company: "Conduit Venture Labs",
   category: "Software Development", 
   skills: [
-    "JavaScript",
-    "Python",
-    "HTML/CSS",
-    "SQL",
+    "TypeScript",
+    "React Native",
+    "PostgreSQL",
     "Git",
-    "REST APIs"
   ],
   tools: [
     "VS Code",
@@ -46,7 +39,7 @@ const Experience = () => {
   return (
     <div className='bg-zinc-950 h-auto w-screen p-16 flex flex-col gap-8'>
       <div className='w-full flex flex-col gap-3'>
-        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-white'>experience</h1>
+        <h1 className='scroll-m-20 text-4xl font-extralight tracking-tight lg:text-8xl text-white'>experience</h1>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
         {/* Top Left */}
