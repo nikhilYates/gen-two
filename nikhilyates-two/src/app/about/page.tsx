fca878a94f951354b7112f0ef7ecbac930356b85
nikhilYates <nikhil.yates@outlook.com>
2024-12-08 00:32:39 -0500

    landing, about, experience complete for review

--- nikhilyates-two/src/app/about/page.tsx
+++ nikhilyates-two/src/app/about/page.tsx
@@ -4,14 +4,14 @@ const About = () => {
 
     return (
         <div id='about' className='bg-zinc-950 h-auto min-h-screen p-16 flex flex-col justify-start gap-8'>
-            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-zinc-500 leading-10'><span className='text-white'>nikhil yates</span> is a <span className='text-white'>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-10'><span className='text-white'>nikhil yates</span> is a <span className='text-white'>
             versatile full-stack engineer and entrepreneur </span> based in <span className='text-white'>Toronto. </span>
              With a solid foundation in computer science and a knack for <span className='text-white'>solving complex problems</span> across <span className='text-white'>various domains</span>, nikhil's
             interests span <span className='text-white'> web3, generative ai, data interactivity, robotics, and UI.</span></h1>
-            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-zinc-500 leading-10'>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-10'>
                 <span className='text-white'>TLDR: </span> i like <span className='text-white'>stuff(building + designing + learning)</span>. how's that for professional
             </h1>
-            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-zinc-500 leading-10'>
+            <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-8xl text-zinc-500 leading-10'>
                 want to collaborate? let's get in <span className='underline text-white'>contact.</span>
             </h1>
 
