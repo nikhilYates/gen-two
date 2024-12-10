76889693bd8d202bc852b065067dddfa6cf0b8a2
nikhilYates <nikhil.yates@outlook.com>
2024-12-10 00:37:43 -0500

    portfolio IDE tabs populated with project data

--- nikhilyates-two/src/app/portfolio/page.tsx
+++ nikhilyates-two/src/app/portfolio/page.tsx
@@ -17,11 +17,21 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
 
 import file from '../../../public/assets/svgs/hero/File.svg'
 import search from '../../../public/assets/svgs/hero/Search.svg'
 import music from '../../../public/assets/svgs/hero/Music.svg'
 import locked from '../../../public/assets/svgs/hero/Locked.svg'
+import play from '../../../public/assets/svgs/hero/Play.svg'
+import paper from '../../../public/assets/svgs/hero/Paper.svg'
+
 
 
 const Portfolio = () => {
@@ -33,23 +43,91 @@ const Portfolio = () => {
                 <h1 className='scroll-m-20 text-4xl font-extralight tracking-tight lg:text-8xl text-white'>portfolio</h1>
             </div>
             <div className='h-full w-full border border-zinc-500 rounded-lg p-4 flex flex-row justify-between gap-4'>
-                <div className='h-full w-[15%] bg-zinc-900 rounded-lg flex flex-col justify-start gap-4'>
+                <div className='h-full w-[15%] flex flex-col justify-start gap-4 border-r border-zinc-500'>
                     <div>
                         <Tabs defaultValue="files" className="w-full">
-                            <TabsList className="grid w-full grid-cols-4 bg-zinc-950 text-white rounded-none">
+                            <TabsList className="grid w-full grid-cols-3 bg-zinc-950 text-white rounded-none">
                                 <TabsTrigger value="files" className="data-[state=active]:bg-zinc-800"><Image priority src={file} alt='file-icon' className='h-5 w-5'/></TabsTrigger>
-                                <TabsTrigger disabled value="search" className="data-[state=active]:bg-zinc-800"><Image priority src={search} alt='search-icon' className='h-5 w-5'/></TabsTrigger>
                                 <TabsTrigger disabled value="fork" className="data-[state=active]:bg-zinc-800"><Image priority src={music} alt='music-icon' className='h-5 w-5'/></TabsTrigger>
                                 <TabsTrigger disabled value="misc" className="data-[state=active]:bg-zinc-800"><Image priority src={locked} alt='locked-icon' className='h-5 w-5'/></TabsTrigger>
                             </TabsList>
                             <TabsContent value='files'>
-                                
+                                <Accordion type='multiple' defaultValue={["item-1"]}>
+                                    <AccordionItem value="item-1">
+                                        <AccordionTrigger>algorithms-and-ai</AccordionTrigger>
+                                        <AccordionContent>
+                                            <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            rag_based_llm.ai
+                                        </AccordionContent>
+                                        <AccordionContent>
+                                            <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            rrt_pathfinding.alg
+                                        </AccordionContent>
+                                        <AccordionContent>
+                                            <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            tf_line_following.ai
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
+                                <Accordion type='multiple' defaultValue={["item-1"]}>
+                                    <AccordionItem value="item-1">
+                                        <AccordionTrigger>saas-and-web</AccordionTrigger>
+                                        <AccordionContent>
+                                            <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            book_gpt.saas
+                                        </AccordionContent>
+                                        <AccordionContent>
+                                        <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            scorpionlabz.web
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
+                                <Accordion type='multiple' defaultValue={["item-1"]}>
+                                    <AccordionItem value="item-1">
+                                        <AccordionTrigger>embd-and-robotics</AccordionTrigger>
+                                        <AccordionContent>
+                                            <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            dc_motor.embd
+                                        </AccordionContent>
+                                        <AccordionContent>
+                                        <Image priority src={play} alt='file-icon' className='h-4 w-4'/>
+                                            kinova_g3.rob
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
+                                <Accordion type='multiple' defaultValue={["item-1"]}>
+                                    <AccordionItem value="item-1">
+                                        <AccordionTrigger>writing</AccordionTrigger>
+                                        <AccordionContent>
+                                            <Image priority src={paper} alt='file-icon' className='h-4 w-4'/>
+                                            llm_tc_gen.ppr
+                                        </AccordionContent>
+                                        <AccordionContent>
+                                        <Image priority src={paper} alt='file-icon' className='h-4 w-4'/>
+                                            health_dt.ppr
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
                             </TabsContent>
                         </Tabs>
                     </div>
                 </div>
-                <div className='h-full w-full border border-zinc-700 rounded-lg p-4'>
+                {/* 
+                    actual data display
+                    should be converted into its own component
+                 */}
+                <div className='h-full w-full flex flex-col gap-4'>
+                    <div className='w-full flex flex-row gap-4 h-[75%]'>
+                        <div className='flex-1 border rounded-lg border-zinc-800'>
+                            
+                        </div>
+                        <div className='flex-1 border rounded-lg border-zinc-800'>
 
+                        </div>
+                    </div>
+                    <div className='w-full border border-zinc-800 rounded-lg h-[25%]'>
+
+                    </div>
                 </div>
             </div>
         </div>
--- nikhilyates-two/src/components/ui/accordion.tsx
+++ nikhilyates-two/src/components/ui/accordion.tsx
@@ -14,7 +14,7 @@ const AccordionItem = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn("border-b", className)}
+    className={cn("border-b-0", className)}
     {...props}
   />
 ))
@@ -28,13 +28,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-start gap-2 text-white py-2 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
     >
-      {children}
       <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      {children}
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
@@ -46,10 +46,12 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden flex flex-row items-center justify-start gap-4 pl-8 text-sm text-white transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("flex flex-row items-center gap-2 pb-2 pt-2", className)}>
+      {children}
+    </div>
   </AccordionPrimitive.Content>
 ))
 
